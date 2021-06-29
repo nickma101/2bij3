714d501090c3d54dbaca4efcd060fee4f2cbb402
nickma101 <nimima95@gmail.com>
2021-06-29 14:57:44 +0200

    amcat scraper

--- add_new_amcatdata.py
+++ add_new_amcatdata.py
@@ -4,7 +4,6 @@ from elasticsearch import Elasticsearch
 amcat = AmcatAPI("https://vu.amcat.nl", "username", "password")
 es = Elasticsearch()
 
-es.indices.delete("inca")
 es.indices.create("inca")
 
 for article in amcat.get_articles(project=69, articleset=2564, start_date="2021-03-09", columns=("date", "title", "publisher", "url", "text", "author", "section")):
